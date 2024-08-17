794e5975fd1b2b93a5fe2a87a6d25d1a78e9d48f
Ikuta-N <2242131e@stu.kobe-u.ac.jp>
2024-08-17 15:06:06 +0900

    compleated (a)

--- 01_cleaning/code_file.R
+++ 01_cleaning/code_file.R
@@ -5,26 +5,22 @@ semdata_1 <- read.csv("raw/semester_dummy/semester_data_1.csv", header = T)
 semdata_2 <- read.csv("raw/semester_dummy/semester_data_2.csv")
 
 semdata_joined <- rbind(semdata_1, semdata_2) %>% 
-  select(-x6) %>% 
+  select(-x6) 
+colnames(semdata_joined) <- semdata_joined[1,]
+semdata <- semdata_joined[-1,] %>% 
+  mutate(semester = as.numeric(semester)) %>% 
+  mutate(quarter = as.numeric(quarter)) %>% 
+  group_by(unitid) %>% 
+  mutate(onlysemester = prod(semester)) %>% 
+  mutate(onlyquarter = prod(quarter)) %>% 
+  ungroup() %>% 
+  mutate(introsem = ifelse(onlysemester == 0 & onlyquarter == 0 & semester == 1, as.numeric(year), 2500)) %>% 
+  group_by(unitid) %>% 
+  mutate(introsem = ifelse(min(introsem) == 2500, NA_real_, min(introsem))) %>% 
+  mutate(aftersem_dummy = ifelse(!is.na(introsem) & semester == 1, 1, 0)) %>% 
+  select(-onlyquarter, -onlysemester) %>% 
+  ungroup()
   
-
-
-
-# semester_dummy_temp <- c("temp")
-# 
-# for (i in (2:nrow(semdata))) {
-#   if((semdata[i,1] == semdata[i-1,1]) && (semdata[i,3] != semdata[i-1,3]) && (semdata[i,3] == 1)){
-#     semester_dummy_temp <- rbind(semester_dummy_temp, 1)
-#   } else  {
-#     semester_dummy_temp<- rbind(semester_dummy_temp, 0)
-#   } 
-# }
-# rownames(semester_dummy_temp) <- (1:nrow(semester_dummy_temp))
-# semdata <- data.frame(semdata_joined, semester_dummy_temp)
-# semdata <- semdata %>% 
-#   mutate(intro_semester = ifelse(semester_dummy_temp == 1, as.numeric(x5), 0)) %>% 
-#   select(-semester_dummy_temp) %>% 
-#   mutate(after_semester_dummy = ifelse(intro_semester >= 2001, 1,0))
 
 #2
 
